693b0dd73abdc623b2df1d44209f4ef911a469e1
mya <myawalkerinfo@gmail.com>
2025-07-21 17:05:27 -0400

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,13 @@
-import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
-import { QueryClientProvider, QueryClient} from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import MainLayout from "./layouts/main-layout";
 import ProtectedRoute from "./components/protected-route";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Todos from "./pages/todos";
-import { useEffect } from "react";
+
 
 const client = new QueryClient();
-
-function RedirectToLogin() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/login")
-  }, [])
-
-  return <></>
-}
 
 function App() {
   return (
@@ -26,10 +16,20 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/todos" element={<MainLayout />}>
+            <Route
+              path="/todos"
+              element={
+                <ProtectedRoute>
+                  <Todos />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
